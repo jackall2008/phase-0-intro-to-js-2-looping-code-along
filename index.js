0851fd84deb18ddaae06a22677663ec6561e8ee1
Julian Patterson <julian.patterson@gmail.com>
2023-01-02 21:36:44 -1000

    commit tree cbc2d2ce5dd919f452ed263b771cc9774e499049

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(friends, eventName) {
+    let newArray = [];
+    for (let i = 0; i < friends.length; i++) {
+        newArray.push(`Thank you, ${friends[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+writeCards(friends);
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
