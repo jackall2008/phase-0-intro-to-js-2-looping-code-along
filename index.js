2d1b4f2fa8747a5a609b2f2109282a6e9a2cacab
Julian Patterson <julian.patterson@gmail.com>
2023-01-13 12:55:09 -0500

    This code passes 3 looping tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const personalizedCards = [];
+    for (let i = 0; i < names.length; i++) {
+       personalizedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return personalizedCards;
+}
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
